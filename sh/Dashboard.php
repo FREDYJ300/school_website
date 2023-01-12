e547d5bd79dde258a5f5475b212aa227f600858a
FREDYJ300 <64916174+FREDYJ300@users.noreply.github.com>
2023-01-12 14:08:03 +0300

    Add files via upload
    initial phase

---
+++ sh/Dashboard.php
@@ -0,0 +1,30 @@
+<?php
+
+echo 'Hello World';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+  <title>KARIBU SHULE</title>
+  <link rel="stylesheet" type="text/css" href="css/bootstrap/css/bootstrap.min.css" />
+  
+  
+</head>
+<body>
+
+
+
+<div class="card" style="width:18rem">      
+    <input type="name">
+	<input type="password">
+</div>
+<h1>This is a heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html> 
+
+
